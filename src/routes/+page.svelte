7a3f26ef95e0910bf9e4b58af51dd2011f94ff69
Nate Babyak <nate.babyak@outlook.com>
2025-12-19 20:56:04 -0500

    add shadcn-svelte button group component and add buttongroup to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,50 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as ButtonGroup from '$lib/components/ui/button-group/index.js';
+	import Download from '@lucide/svelte/icons/download';
 	import Footer from '$lib/components/footer.svelte';
+	import Github from '$lib/components/github.svelte';
 	import Header from '$lib/components/header.svelte';
+	import Linkedin from '$lib/components/linkedin.svelte';
 </script>
 
 <Header />
 <main>
 	<article>
-		<h1>Nate Babyak</h1>
-		<p>Hi, I'm Nate Babyak</p>
+		<h1 class="text-center text-5xl">Nate Babyak</h1>
+		<p class="text-center text-lg">Software Engineer</p>
+		<p class="">
+			Hi, I'm Nate Babyak. I enjoy turning data into software others find genuinely useful.
+		</p>
+		<ButtonGroup.Root>
+			<ButtonGroup.Root>
+				<Button href="https://github.com/natebabyak" target="_blank" variant="outline">
+					<Github />
+					GitHub
+				</Button>
+				<Button href="https://www.linkedin.com/in/natebabyak/" target="_blank" variant="outline">
+					<Linkedin />
+					LinkedIn
+				</Button>
+			</ButtonGroup.Root>
+			<ButtonGroup.Root>
+				<Button>
+					<Download />
+					Resume
+				</Button>
+			</ButtonGroup.Root>
+		</ButtonGroup.Root>
 		<section>
-			<h2>Projects</h2>
+			<h2 id="projects" class="font-serif text-3xl">Projects</h2>
 		</section>
 		<section>
-			<h2>Experience</h2>
+			<h2 id="experience" class="font-serif text-3xl">Experience</h2>
 		</section>
 		<section>
-			<h2>Education</h2>
+			<h2 id="education" class="font-serif text-3xl">Education</h2>
 		</section>
 		<section>
-			<h2>Contact</h2>
+			<h2 id="contact" class="font-serif text-3xl">Contact</h2>
 		</section>
 	</article>
 </main>
